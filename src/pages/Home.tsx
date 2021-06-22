dc05cdd23a65b329d302733fd46206e28da1db05
Tiago Gonçalves <tiaguin180@gmail.com>
2021-06-22 15:33:40 -0300

    feat: signIn from google

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -6,11 +6,17 @@ import googleIconImg from '../assets/images/google-icon.svg';
 
 import '../styles/auth.scss';
 import { Button } from '../components/Button';
+import { useAuth } from '../hooks/useAuth';
 
 export function Home() {
   const history = useHistory();
+  const { user, signInWithGoogle } = useAuth();
 
-  function handleCreateRoom() {
+  async function handleCreateRoom() {
+    if(!user) {
+      await signInWithGoogle()
+    }
+
     history.push('/rooms/new');
   }
 
