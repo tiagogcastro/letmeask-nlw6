ceaed4b16c126ac7d6d267142490fdc73704f9fc
Tiago Gonçalves <tiaguin180@gmail.com>
2021-06-25 10:31:40 -0300

    feat: Added admin router

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { AuthContextProvider } from './contexts/AuthContext';
+
 import { Home } from './pages/Home';
 import { NewRoom } from './pages/NewRoom';
 import { Room } from './pages/Room';
-
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
-import { AuthContextProvider } from './contexts/AuthContext';
+import { AdminRoom } from './pages/AdminRoom';
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
           <Route path="/" exact component={Home} />
           <Route path="/rooms/new" component={NewRoom} />
           <Route path="/rooms/:id" component={Room} />
+          <Route path="/admin/rooms/:id" component={AdminRoom} />
         </Switch>
       </AuthContextProvider>
     </BrowserRouter>
