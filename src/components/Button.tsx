c1498718198fc09f0e7a954f523f67041f5f53da
Tiago Gonçalves <tiaguin180@gmail.com>
2021-06-25 10:32:52 -0300

    feat: Added outlined

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -2,13 +2,19 @@ import { ButtonHTMLAttributes } from 'react';
 
 import '../styles/button.scss';
 
-type ButtonProps = ButtonHTMLAttributes<HTMLButtonElement>;
+type ButtonProps = ButtonHTMLAttributes<HTMLButtonElement> & {
+  isOutlined?: boolean;
+  
+};
 
-export const Button = (props: ButtonProps) => {
+export const Button = ({isOutlined = false, ...props}: ButtonProps) => {
 
   return (
     <>
-      <button className="button" {...props} />
+      <button 
+        className={`button ${isOutlined ? 'outlined' : ''}`} 
+        {...props} 
+      />
     </>
   );
 }
