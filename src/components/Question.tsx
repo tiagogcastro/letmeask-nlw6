2b8ae41189257196049fa998d8b206af90ff4d69
Tiago Gonçalves <tiaguin180@gmail.com>
2021-06-25 10:33:05 -0300

    feat: Added children on question

--- src/components/Question.tsx
+++ src/components/Question.tsx
@@ -1,3 +1,4 @@
+import { ReactNode } from 'react';
 import '../styles/question.scss';
 
 type QuestionProps = {
@@ -5,13 +6,14 @@ type QuestionProps = {
   author: {
     name: string;
     avatar: string;
-
-  }
+  };
+  children?: ReactNode;
 }
 
 export function Question({
   content,
-  author
+  author,
+  children
 }: QuestionProps) {
   return (
     <div className="question">
@@ -21,7 +23,9 @@ export function Question({
           <img src={author.avatar} alt={author.name} />
           <span>{author.name}</span>
         </div>
-        <div></div>
+        <div>
+          {children}
+        </div>
       </footer>
     </div>
   );
