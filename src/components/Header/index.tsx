fe173f4695324ed1d03cd15d3e1629b404ede64f
Tiago Gonçalves <tiaguin180@gmail.com>
2021-06-27 15:51:53 -0300

    feat: Created Header component

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,23 @@
+import { ReactNode } from 'react';
+import { Link } from 'react-router-dom';
+
+import logoImg from '../../assets/images/logo.svg';
+
+import './styles.scss';
+
+type HeaderProps = {
+  children?: ReactNode;
+}
+
+export function Header({children}: HeaderProps) {
+  return (
+    <header>
+      <div className="content">
+        <Link to="/">
+          <img src={logoImg} alt="Logo do letmeask" />
+        </Link>
+        {children}
+      </div>
+    </header>
+  );
+}
