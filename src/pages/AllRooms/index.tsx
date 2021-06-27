8cc3f8fd480cf11fda94fb3ac71e9efde8b33868
Tiago Gonçalves <tiaguin180@gmail.com>
2021-06-27 15:52:18 -0300

    feat: Initialized all rooms page

---
+++ src/pages/AllRooms/index.tsx
@@ -0,0 +1,13 @@
+
+import { Header } from '../../components/Header';
+import './styles.scss';
+
+export function AllRooms() {
+  return (
+    <div id="page-allRooms">
+      <Header>
+        
+      </Header>
+    </div>
+  )
+}
