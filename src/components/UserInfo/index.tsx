e5aa4cdf653804814db2351af64004d8b998cbdf
Tiago Gonçalves <tiaguin180@gmail.com>
2021-06-30 15:43:05 -0300

    refactor: Using props at userInfo

--- src/components/UserInfo/index.tsx
+++ src/components/UserInfo/index.tsx
@@ -1,5 +1,3 @@
-import { useAuth } from '../../hooks/useAuth';
-
 import './styles.scss';
 
 type UserInfoProps = {
@@ -8,17 +6,10 @@ type UserInfoProps = {
 }
 
 export function UserInfo(props: UserInfoProps) {
-  const {user} = useAuth();
-
-  const userProps = {
-    avatar: user?.avatar || props.avatar,
-    name: user?.name || props.name,
-  };
-  
   return (
     <div className="user-info">
-      <img src={userProps.avatar} alt={userProps.name} />
-      <span>{userProps.name}</span>
+      <img src={props.avatar} alt={props.name} />
+      <span>{props.name}</span>
     </div>
   )
 }
